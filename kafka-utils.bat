eb30c62a16f80d1f127f053b1a5ee8656dec1166
Jesus Ramirez <drjesusf@gmail.com>
2025-01-02 17:24:43 -0500

    Update kafka-utils.bat to add publisher command and configure KAFKA for minikube access

--- kafka-utils.bat
+++ kafka-utils.bat
@@ -10,15 +10,13 @@ Start-Process -NoNewWindow -FilePath "$env:KAFKA_HOME\bin\windows\kafka-server-s
 REM Listar los topicos de kafka desde PowerShell
 & "$env:KAFKA_HOME\bin\windows\kafka-topics.bat" --list --bootstrap-server localhost:9092
 
+REM Crear un publisher en kafka desde PowerShell
+echo "Mensaje 1" | & "$env:KAFKA_HOME\bin\windows\kafka-console-producer.bat" --broker-list localhost:9092 --topic test-topic
+
+
 REM Crear un consumer en kafka desde PowerShell
 & "$env:KAFKA_HOME\bin\windows\kafka-console-consumer.bat" --bootstrap-server localhost:9092 --topic test-topic --from-beginning
 
-REM Listar los topicos de kafka
-C:\kafka\kafka_2.13-3.8.1\bin\windows\kafka-topics.bat --list --bootstrap-server localhost:9092
-
-REM Crear un nuevo tópico en kafka desde PowerShell
-"$env.KAFKA_HOME\bin\windows\kafka-topics.bat --create --topic nuevo-topico --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1"
-
-REM Crear un nuevo tópico en kafka desde el archivo .bat
-%KAFKA_HOME%\bin\windows\kafka-topics.bat --create --topic nuevo-topico --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
-
+REM Configurar KAFKA para que pueda ser accedido desde minikube
+En el archivo server.properties de KAFKA, cambiar la propiedad advertised.listeners a la ip de la maquina virtual de minikube
+advertised.listeners=PLAINTEXT://192.168.18.199:9092
